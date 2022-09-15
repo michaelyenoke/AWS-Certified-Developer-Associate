179c3f47d62df56d5e4f9a908c5fdf3eb5100c8b
Y.H. Chen <michaelyenoke@hotmail.com>
2022-09-16 03:52:54 +0800

    Update lambda_code.py

--- 38. AWS Lambda Versioning and alias from the CLI/lambda_code.py
+++ 38. AWS Lambda Versioning and alias from the CLI/lambda_code.py
@@ -4,7 +4,7 @@ import json
 from botocore.exceptions import ClientError
 def handler(event, context):
     s3 = boto3.resource('s3', region_name='us-east-1')
-    s3.create_bucket(Bucket='whizlabs33194.37699701')
+    s3.create_bucket(Bucket='bucket-name')
     content="File uploaded by version 1"
-    responses3 = s3.Object('whizlabs33194.37699701', 'version1.txt').put(Body=content)
+    responses3 = s3.Object('object-name', 'version1.txt').put(Body=content)
     print("uploading file completed")
