bab2d89a1944c583c5989aa5e0081772921dcf98
Y.H. Chen <michaelyenoke@hotmail.com>
2022-09-16 04:10:40 +0800

    Create ec2_code_create_lambda_version2.py

---
+++ 38. AWS Lambda Versioning and alias from the CLI/ec2_code_create_lambda_version2.py
@@ -0,0 +1,10 @@
+import logging
+import boto3
+import json
+from botocore.exceptions import ClientError
+def handler(event, context):
+    s3 = boto3.resource('s3', region_name='us-east-1')
+    s3.create_bucket(Bucket='bucket-name')
+    content="File uploaded by version 2"
+    responses3 = s3.Object('object-name', 'version2.txt').put(Body=content)
+    print("uploading file completed")
