4f50fdc92f0e6402e2cd5474a18b4f77a607d87a
Y.H. Chen <michaelyenoke@hotmail.com>
2022-09-18 16:24:43 +0800

    Update ec2_function.py

--- 47. Deploying Lambda Functions using CloudFormation/ec2_function.py
+++ 47. Deploying Lambda Functions using CloudFormation/ec2_function.py
@@ -4,4 +4,4 @@ import json
 from botocore.exceptions import ClientError
 def lambda_handler(event, context):
     client = boto3.client('ec2')
-    response = client.run_instances(ImageId='ami-0742b4e673072066f', InstanceType='t2.micro', MaxCount=1, MinCount=1)
+    response = client.run_instances(ImageId='ami-number', InstanceType='t2.micro', MaxCount=1, MinCount=1)
